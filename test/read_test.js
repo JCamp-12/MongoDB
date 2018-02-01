00273cc196582ef06e2bdacb635d2495f7ec15aa
JCamp-12 <JChandlerCampbell@gmail.com>
2018-02-01 03:34:10 -0500

    User.find our test user works

---
+++ test/read_test.js
@@ -0,0 +1,20 @@
+const assert = require('assert');
+const User = require('../src/user');
+
+describe('Reading users out of the db', () => {
+  let jason;
+
+  beforeEach((done) => {
+    jason = new User({ name: 'Jason' });
+    jason.save()
+      .then(() => done())
+    });
+
+  it('finds users with a name of jason', (done) => {
+    User.find({ name: 'Jason' })
+      .then((users) => {
+        console.log(users);
+        done();
+      });
+  });
+});
